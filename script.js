61574c4ec01a2270b31b74b8db1247830907b6e1
dymjin <dymjin@gmail.com>
2023-11-16 13:50:42 +0200

    Add DOMdisplay* + edit other functions** *DOMdisplay renders board + updates display when new move is made **Gameboard no longer handles updating of display, but does convert base divs to 2d arr. Any function mentioning the console has been updated to use DOM elements instead. endGame func now removes event listener if winner has been determined

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 const Gameboard = (() => {
-    let gameboardArr = [
-        ["b", "b", "b"],
-        ["b", "b", "b"],
-        ["b", "b", "b"]
-    ];
-
-    const display = () => {
-        let msg = "";
-        for (let i = 0; i < gameboardArr.length; i++) {
-            for (let j = 0; j < gameboardArr[i].length; j++) {
-                if (j % 3 === 0) msg += `\n`;
-                msg += `${gameboardArr[i][j]} `;
+    let gameboardArr = [[], [], []];
+    for (let j = 0; j < gameboardArr.length;) {
+        for (let i = 0; i < 9; i++) {
+            if (i % 3 !== 0) {
+                gameboardArr[j - 1].push(document.querySelectorAll(".container>div")[i]);
+            } else {
+                j++;
+                gameboardArr[j - 1].push(document.querySelectorAll(".container>div")[i]);
             }
         }
-        console.log(msg);
     }
-
     const getBoard = () => gameboardArr;
 
-    return { display, getBoard };
+    return { getBoard };
 })();
 
 function Player(name, marker) {
@@ -36,7 +30,26 @@ function Player(name, marker) {
     return { getName, getMarker, updateScore, getScore };
 }
 
-const Game = () => {
+const DOMDisplay = (() => {
+    const board = Gameboard.getBoard();
+    const updateDisplay = (div, marker) => {
+        if (marker === "O") div.textContent = "circle";
+        else div.textContent = "close";
+    }
+
+    const clearDisplay = () => {
+        board.forEach(arr => {
+            arr.forEach(div => div.addEventListener('click', () => {
+                div.textContent = "";
+            }))
+        })
+    }
+
+    return { updateDisplay, clearDisplay };
+})();
+
+const Game = (() => {
+    const board = Gameboard.getBoard();
     let currentPlayer;
     let marker2;
     let moves = [];
@@ -49,44 +62,40 @@ const Game = () => {
             player1,
             player2
         ];
-
     if (players[0].getMarker() === "X") {
         currentPlayer = players[0];
-        console.log(`${players[0].getName()}'s turn`);
     }
     else {
         currentPlayer = players[1];
-        console.log(`${players[1].getName()}'s turn`);
     }
 
     const getCurrentPlayer = () => currentPlayer;
 
     const switchPlayerTurn = () => {
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
-        console.log(`${getCurrentPlayer().getName()}'s turn`);
     }
 
     const checkBoard = (board) => {
         for (let x = 0; x < (board.length - 2); x++) {
             for (let y = 0; y < board.length; y++) {
-                if (board[x][y] !== "b"
-                    && board[x][y] === board[x + 1][y]
-                    && board[x + 2][y] === board[x + 1][y]) {
+                if (board[x][y].textContent !== ""
+                    && board[x][y].textContent === board[x + 1][y].textContent
+                    && board[x + 2][y].textContent === board[x + 1][y].textContent) {
                     return true;
                 }
-                else if (board[y][x] !== "b"
-                    && board[y][x] === board[y][x + 1]
-                    && board[y][x + 1] === board[y][x + 2]) {
+                else if (board[y][x].textContent !== ""
+                    && board[y][x].textContent === board[y][x + 1].textContent
+                    && board[y][x + 1].textContent === board[y][x + 2].textContent) {
                     return true;
                 }
-                else if (board[x][x] !== "b"
-                    && board[x][x] === board[x + 1][x + 1]
-                    && board[x + 1][x + 1] === board[x + 2][x + 2]) {
+                else if (board[x][x].textContent !== ""
+                    && board[x][x].textContent === board[x + 1][x + 1].textContent
+                    && board[x + 1][x + 1].textContent === board[x + 2][x + 2].textContent) {
                     return true;
                 }
-                else if (board[x][x + 2] !== "b"
-                    && board[x][x + 2] === board[x + 1][x + 1]
-                    && board[x + 1][x + 1] === board[x + 2][x]) {
+                else if (board[x][x + 2].textContent !== ""
+                    && board[x][x + 2].textContent === board[x + 1][x + 1].textContent
+                    && board[x + 1][x + 1].textContent === board[x + 2][x].textContent) {
                     return true;
                 }
             }
@@ -95,35 +104,31 @@ const Game = () => {
     }
 
     const endGame = () => {
-        console.clear();
-        console.log(Gameboard.display());
-        console.log(`${getCurrentPlayer().getName()} won this round!`);
+        controller.abort();
         getCurrentPlayer().updateScore();
     }
-
-    const placeMarker = (x, y) => {
-        const board = Gameboard.getBoard();
-        if (x > board.length || y > board.length) {
-            console.log('invalid move!');
-            console.log(`${getCurrentPlayer().getName()}'s turn`);
-        }
-        else {
-            if (board[x - 1][y - 1] !== "b") {
-                console.log('marker already present!');
-                console.log(`${getCurrentPlayer().getName()}'s turn`);
-            } else {
-                board[x - 1].splice(y - 1, 1, getCurrentPlayer().getMarker());
-                Gameboard.display();
-                moves.push(getCurrentPlayer().getMarker());
-                switchPlayerTurn();
+    
+    //https://developer.mozilla.org/en-US/docs/Web/API/AbortController
+    //https://stackoverflow.com/questions/3723914/remove-eventlistener-in-javascript-after-event-occurred#:~:text=For%20those%20who%20needs%20to%20remove%20after%20a%20certain%20condition%20(or%20even%20inside%20a%20loop%20too)%2C%20one%20alternative%20is%20using%20AbortController%20and%20AbortSignal%3A
+    const controller = new AbortController();
+    const placeMarker = () => {
+        board.forEach(arr => arr.forEach(div => {
+            checkMoves = (div) => {
+                return { div };
             }
-        }
-        if (moves.length >= 5 && checkBoard(board)) {
-            endGame();
-        }
+            div.addEventListener('click', () => {
+                checkMoves.div = div;
+                if (div.textContent !== "") { }
+                else {
+                    DOMDisplay.updateDisplay(div, getCurrentPlayer().getMarker());
+                    moves.push(getCurrentPlayer().getMarker());
+                    if (moves.length >= 5 && checkBoard(board)) {
+                        endGame();
+                    }
+                    switchPlayerTurn();
+                }
+            }, { signal: controller.signal });
+        }));
     }
-
-    return { getCurrentPlayer, placeMarker };
-};
-
-const game = Game();
+    placeMarker();
+})();
